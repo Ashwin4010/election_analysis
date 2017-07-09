0172509a5920a72c1eb847242d55cf82c73f5634
sachinsanke <sachin.sanke@gmail.com>
2017-07-09 18:26:29 +0530

    Add files via upload

--- src/com/sachin/election/constants/DataFileConstant.java
+++ src/com/sachin/election/constants/DataFileConstant.java
@@ -1,1 +1,21 @@
+package com.sachin.election.constants;
 
+public class DataFileConstant {
+	public static final int STATE_NAME = 0;
+	public static final int YEAR = 1;
+	public static final int ASSEMBLY_CONSTITUENCY_NO = 2;
+	public static final int ASSEMBLY_CONSTITUENCY_NAME = 3;
+	public static final int ASSEMBLY_CONSTITUENCY_TYPE = 4;
+	
+	public static final int CANDIDATE_NAME = 5;
+	public static final int CANDIDATE_SEX = 6;
+	public static final int PARTY_NAME = 7;
+	public static final int PARTY_ABBREVATION = 8;
+	public static final int TOTAL_VOTE_POLLED = 9;
+	public static final int NO_OF_ELECTORS = 10;
+	
+	public static final String FILE_SEP = "\t";
+	public static final String KEY_SEP="\t";
+	
+						
+}
